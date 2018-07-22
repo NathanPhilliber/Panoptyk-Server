ec5b1bee432c5bf82d4bbc53c77bb26ae80c1963
NathanPhilliber <nphilliber@gmail.com>
2018-07-22 23:41:14 +0000

    Test interface / socket setup

--- communication/socket_inputs.js
+++ communication/socket_inputs.js
@@ -1,3 +1,10 @@
 server.modules.io.on('connection', function(socket) {
+  server.logger.log('Client Connected');
+
+  socket.on('test', function(data) {
+    server.logger.log('Test message received');
+    data.timeReceived = (new Date()).toISOString();
+    socket.emit('Message Received', data);
+  });
 
 });
