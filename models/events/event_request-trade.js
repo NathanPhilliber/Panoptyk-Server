22d12ed34145a95084d7539667554b80ab28310c
NathanPhilliber <nphilliber@gmail.com>
2018-10-01 05:50:20 +0000

    trade events

--- models/events/event_request-trade.js
+++ models/events/event_request-trade.js
@@ -1,11 +1,56 @@
-server.models.Event_requestTrade = {};
+/**
+ * Event model.
+ * @param {Object} socket - socket.io client socket object.
+ * @param {Object} inputData - raw input recieved.
+ */
+function Event_requestTrade(socket, inputData) {
+  this.time = new Date();
+  this.agent = server.models.Agent.get_agent_by_socket(socket);
 
-server.models.Event_requestTrade.event_name = "request-trade";
+  if (!(res = server.models.Event_requestTrade.validate(inputData, this.agent)).status) {
+    server.log('Bad event requestTrade data ('+JSON.stringify(inputData) + ').', 1);
+    server.send.event_failed(socket, server.models.Event_requestTrade.event_name, res.message);
+    return false;
+  }
 
-server.models.Event_requestTrade.formats = [{
-    "agent_id": "number"
-  }];
+  this.cnode = res.cnode;
+  this.to_agent = res.to_agent;
 
-server.models.Event_requestTrade.validate = function(structure) {
-  return server.models.Event.validate_key_format(server.models.Event_requestTrade.formats, structure);
+  //server.control.add_agent_to_cnode(this.cnode, this.agent);
+
+  (server.models.Event.objects = server.models.Event.objects || []).push(this);
+  server.log('Event request-trade (' + this.cnode.cnode_id + ') for agent ' + this.agent.name + ' registered.', 2);
+}
+
+Event_requestTrade.event_name = 'request-trade';
+
+Event_requestTrade.formats = [{
+  'agent_id': 'number'
+}]
+
+
+/**
+ * Event validation.
+ * @param {Object} structure - raw input recieved.
+ * @param {Object} agent - agent associated with this event.
+ * @return {Object}
+ */
+Event_requestTrade.validate = function(structure, agent) {
+
+  if (!(res = server.models.Event.validate_agent_logged_in(agent)).status) {
+    return res;
+  }
+  if (!(res = server.models.Event.validate_key_format(server.models.Event_requestTrade.formats, structure)).status) {
+    return res;
+  }
+  var to_agent = server.models.Agent.get_agent_by_id(structure.agent_id);
+  if (!(res = server.models.Event.validate_agent_logged_in(to_agent)).status) {
+    return res;
+  }
+  if (!(res = server.models.Event.validate_agents_share_cnode(agent, to_agent)).status) {
+    return res;
+  }
+  return res;
 };
+
+server.models.Event_requestTrade = Event_requestTrade;
