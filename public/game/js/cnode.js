afbbd03128a767f1d2cce3f2234a0c8bc39938a5
Nathan <nphilliber@gmail.com>
2018-10-02 09:18:05 -0700

    client sync

--- public/game/js/cnode.js
+++ public/game/js/cnode.js
@@ -4,11 +4,6 @@ function Cnode(cnode_id, max_agents, agent_ids) {
   this.cnode_id = cnode_id;
   this.max_agents = max_agents;
 
-  this.agents = [];
-  for (let agent_id of agent_ids) {
-    this.agents.push(current_room.get_agent(agent_id));
-  }
-
   this.sprite = graphics.add.sprite(Math.random()*(game.canvas.width-200)+100, Math.random()*(game.canvas.height-200)+100, 'cnode');
   this.sprite.setOrigin(0.5, 0.5);
   this.sprite.setInteractive();
@@ -19,6 +14,12 @@ function Cnode(cnode_id, max_agents, agent_ids) {
     console.log('entering cnode ' + cnode_id);
     Client.send.joinCnode(cnode);
   });
+
+  this.agents = [];
+  for (let agent_id of agent_ids) {
+    //this.agents.push(current_room.get_agent(agent_id));
+    this.add_agent(current_room.get_agent(agent_id));
+  }
 
   console.log("Cnode " + this.cnode_id + " initialized.");
 }
